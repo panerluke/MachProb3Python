afa997312d65e0739eba245a76c0773f7a870d18
panerluke <47804291+panerluke@users.noreply.github.com>
2019-12-07 22:44:03 +0800

    Add files via upload

--- prob3.py
+++ prob3.py
@@ -1,7 +1,12 @@
 import numpy as np
 
 def prob3(f):
-    for n in range(len(f)):
+    if len(f) <= 10:
+        fn = len(f)
+    else:
+        fn = 10
+    
+    for n in range(fn):
         p = np.polyfit(f[:, 0], f[:, 1], n)
         e = np.linalg.norm(f[:, 1] - np.polyval(p, f[:, 0]))
         x = [n, e]
@@ -9,5 +14,7 @@ def prob3(f):
             y = x
         if y[1] >= x[1]:
             z = x[0]
+            
     p = np.polyfit(f[:, 0], f[:, 1], z)
+    
     return p
